f0d79fb7191603c30192d0ce35e43e5f4c7f935c
axelhoy <121445908+axelhoy@users.noreply.github.com>
2025-06-04 12:19:27 -0300

    Agregado carrusel responsive a productos, modificado JS para creacion de cards, modificado diseño de productos

--- FrontEnd/js/productos.js
+++ FrontEnd/js/productos.js
@@ -24,16 +24,21 @@ function agregarAlCarrito(producto) {
 function mostrarProductos() {
     const auricularesDiv = document.getElementById("categoria-auriculares");
     const tecladosDiv = document.getElementById("categoria-teclados");
+    const carruselAuriculares = document.getElementById("carousel-inner-auriculares");
+    const carruselTeclados = document.getElementById("carousel-inner-teclados");
 
     const productos = obtenerTodosLosProductos().filter(p => p.activo !== false);
 
     if (productos.length === 0) {
-        auricularesDiv.innerHTML = "<p>No hay productos disponibles.</p>";
-        tecladosDiv.innerHTML = "<p>No hay productos disponibles.</p>";
+        auricularesDiv.innerHTML = tecladosDiv.innerHTML = "<p>No hay productos disponibles.</p>";
         return;
     }
 
+    let indexAur = 0;
+    let indexTec = 0;
+
     productos.forEach(producto => {
+        // Card para grilla
         const card = document.createElement("div");
         card.className = "card-producto";
         card.innerHTML = `
@@ -42,13 +47,32 @@ function mostrarProductos() {
             <p>Precio: $${producto.precio}</p>
             <button class="btn-agregar">Agregar al carrito</button>
         `;
-
         card.querySelector(".btn-agregar").addEventListener("click", () => agregarAlCarrito(producto));
+
+        // Card para carrusel
+        const carouselItem = document.createElement("div"); // Creo item para carrusel
+        carouselItem.className = `carousel-item ${(producto.categoria === "auriculares" && indexAur === 0) || 
+                                                  (producto.categoria === "teclados" && indexTec === 0) ? 'active' : ''}`; // con el active muestra primero el primer item de productos
+        carouselItem.innerHTML = `
+            <div class="card mx-auto" style="width: 18rem;">
+                <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">Precio: $${producto.precio}</p>
+                    <button class="btn btn-primary btn-agregar">Agregar al carrito</button>
+                </div>
+            </div>
+        `;
+        carouselItem.querySelector(".btn-agregar").addEventListener("click", () => agregarAlCarrito(producto));
 
         if (producto.categoria === "auriculares") {
             auricularesDiv.appendChild(card);
-        } else {
+            carruselAuriculares.appendChild(carouselItem);
+            indexAur++; // le agrego indice asi lo diferencia del primer item
+        } else if (producto.categoria === "teclados") {
             tecladosDiv.appendChild(card);
+            carruselTeclados.appendChild(carouselItem);
+            indexTec++;
         }
     });
 }
