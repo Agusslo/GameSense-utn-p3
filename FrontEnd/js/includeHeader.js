da644c359a3dd58ed90562ef6e0706a0b5f8a2a2
axelhoy <121445908+axelhoy@users.noreply.github.com>
2025-06-13 11:16:54 -0300

    Agregado boton config

--- FrontEnd/js/includeHeader.js
+++ FrontEnd/js/includeHeader.js
@@ -2,4 +2,29 @@ fetch("header.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("header-container").innerHTML = data;
+    configModal();
   });
+
+
+function configModal() {
+    const configBtn = document.getElementById("btn-config");
+    const modal = document.getElementById("configModal");
+    const closeBtn = document.querySelector(".close-config");
+
+    if (configBtn && modal && closeBtn) {
+      configBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        modal.style.display = "block";
+      });
+
+      closeBtn.addEventListener("click", function () {
+        modal.style.display = "none";
+      });
+
+      window.addEventListener("click", function (e) {
+        if (e.target === modal) {
+          modal.style.display = "none";
+        }
+      });
+    }
+  }
