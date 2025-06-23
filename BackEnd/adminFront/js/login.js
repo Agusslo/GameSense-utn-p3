729a5ae1d98f9ccbaf998c92cf9173e81a7d32ac
Agusslo <agus.lopez042004@gmail.com>
2025-06-23 09:01:58 -0300

    agrego inicio rapido en login del admin

--- BackEnd/adminFront/js/login.js
+++ BackEnd/adminFront/js/login.js
@@ -5,10 +5,15 @@ const correo = document.getElementById("correo").value;
 const contrasena = document.getElementById("contrasena").value;
 const mensajeError = document.getElementById("mensajeError");
 
-if (correo.endsWith("@admin.com") && contrasena === "1234") {
+if (correo.endsWith("@admin.com") && contrasena === "admin1234") {
     mensajeError.textContent = "";
     window.location.href = "dashboard.html";
 } else {
     mensajeError.textContent = "Correo o contraseña incorrectos";
 }
 });
+
+function autocompletarLogin() {
+    document.getElementById("correo").value = "admin@admin.com";
+    document.getElementById("contrasena").value = "admin1234";
+}
